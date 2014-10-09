ae1a8e23f08b5c7764d185efb3324dcf39c4aa4d
Blake Arnold <arnold.blake@gmail.com>
2014-10-09 17:31:53 -0400

    Update bootloader.asm

--- source/bootloader/bootloader.asm
+++ source/bootloader/bootloader.asm
@@ -12,30 +12,30 @@
 ; ------------------------------------------------------------------
 ; FAT12 Boot Sector BIOS parameter block
 ; ------------------------------------------------------------------
-  db 0, 0		; Ignored (8 - 10)
-  dw 512		; Bytes per logical sector
-  db 1			; Logical sectors per cluster
-  dw 1			; Reserved logical sectors
-  db 2			; Number of FATs
-  dw 224		; Root directory entries
-  dw 2880		; Total logical sectors
-  db 0			; Media descriptor
-  dw 9			; Logical sectors per FAT
-  dw 18			; Physical sectors per track
-  dw 2			; Number of heads
-  dd 0			; Hidden sectors
-  dd 0			; Large total logical sectors
-  db 0			; Physical drive number
-  db 0			; Flags etc.
-  db 41			; Boot signature
-  dd __POSIX_TIME__	; Volume serial number
-  db "           "	; Volume label
-  db "FAT12   "		; File system type
+	db 0, 0				; Ignored (8 - 10)
+	dw 512				; Bytes per logical sector
+	db 1				; Logical sectors per cluster
+	dw 1				; Reserved logical sectors
+	db 2				; Number of FATs
+	dw 224				; Root directory entries
+	dw 2880				; Total logical sectors
+	db 0				; Media descriptor
+	dw 9				; Logical sectors per FAT
+	dw 18				; Physical sectors per track
+	dw 2				; Number of heads
+	dd 0				; Hidden sectors
+	dd 0				; Large total logical sectors
+	db 0				; Physical drive number
+	db 0				; Flags etc.
+	db 41				; Boot signature
+	dd __POSIX_TIME__	; Volume serial number
+	db "           "	; Volume label
+	db "FAT12   "		; File system type
 ; ------------------------------------------------------------------
 
 bootloader:
-  mov [boot_device], dl	; Store boot_device
-  jmp $
+	mov [boot_device], dl	; Store boot_device
+	jmp $
 
 %include "../lib/lba2chs.asm"
 
@@ -44,5 +44,5 @@ bootloader:
 
 ; ------------------------------------------------------------------
 ; Pad out remaining space and set the bootsignature bytes
-    times 510-($-$$) db 0	; Pad with zero
-    dw 0AA55h		        ; Boot signature
+	times 510-($-$$) db 0	; Pad with zero
+	dw 0AA55h		        ; Boot signature
