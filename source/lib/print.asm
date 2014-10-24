17fadd0c8433f83d3940030fb12312b3ebd9b8d9
Blake Arnold <arnold.blake@gmail.com>
2014-10-24 14:00:50 -0400

    new print function

--- source/lib/print.asm
+++ source/lib/print.asm
@@ -2,18 +2,17 @@
 ; FILE: print.asm AUTHOR: Blake Arnold DATE: 10-10-2014
 ; PURPOSE: Uses BIOS interupt 0x10 to print a null terminated 
 ; string to the output.
-; IN AX (address of null terminated string)
+; PARAMETERS: SI Null terminated string to print
 ; ==================================================================
 
-print_string:
-	pusha
-	mov bx, ax
-	mov ah, 0x0E
-print_string_start:
-	mov al, [bx]
-	int 0x10
-	add bx, 1
-	cmp byte [bx], 0
-	jne print_string_start
-	popa
+print:
+	push	ax
+	lodsb
+	or	 	al, al
+	jz		end
+	mov 	ah, 0x0e
+	int 	0x10
+	jmp 	print
+.end:
+	pop		ax
 	ret
